41078e0023f0635e85030803c697c111d068d2ad
Ian Harvey <ith.harvey@gmail.com>
2017-07-19 14:59:47 -0700

    changed coors

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use(session({
 
 // CORS Cross Domain
 function allowCrossDomain(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8100');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header(
     'Access-Control-Allow-Headers',
